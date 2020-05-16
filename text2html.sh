88750c99deaae6da761425881d4de32518505564
Medya Gh <medya.gh@gmail.com>
2020-05-16 02:13:38 -0700

    add time sleep

--- text2html.sh
+++ text2html.sh
@@ -3,4 +3,6 @@
 ## TODO: make a for loop and geneate html for all the .txt files
 mkdir -p /data
 go tool test2json -t < /data/testout.txt > /data/testout.json 
+## for slow systems we let the file be saved.
+sleep 3
 gopogh -in /data/testout.json -out /data/testout.html "$NAME" -repo "$REPO"  -details "$DETAILS"
