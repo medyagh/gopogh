2777bc0e1bb3c7e9ed444e791e272683b541aad8
Medya Gh <medya.gh@gmail.com>
2020-05-16 01:55:17 -0700

    create dir

--- text2html.sh
+++ text2html.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ## TODO: make a for loop and geneate html for all the .txt files
-
+mkdir -p /data
 go tool test2json -t < /data/testout.txt > /data/testout.json 
-
 gopogh -in /data/testout.json -out /data/testout.html "$NAME" -repo "$REPO"  -details "$DETAILS"
