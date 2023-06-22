6537101e6f0590e2357d53a664d201bb464a3d59
Judah Nouriyelian <judahnour@gmail.com>
2023-06-22 21:33:50 +0000

    change function name

--- cmd/gopogh/main.go
+++ cmd/gopogh/main.go
@@ -61,7 +61,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if isDBVarSet(*dbPath, *dbBackend) {
+	if dbVarProvided(*dbPath, *dbBackend) {
 		if err := c.SQL(*dbPath, *dbBackend); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -99,8 +99,8 @@ func main() {
 	}
 }
 
-// isDBVarSet checks whether any of the database flags/environment variables are set
-func isDBVarSet(dbPath string, dbBackend string) bool {
+// dbVarProvided checks whether any of the database flags/environment variables are set
+func dbVarProvided(dbPath string, dbBackend string) bool {
 	return (dbPath != "" || os.Getenv("DB_PATH") != "") ||
 		(dbBackend != "" || os.Getenv("DB_BACKEND") != "")
 
