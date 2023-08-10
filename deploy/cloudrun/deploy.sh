7a85977ed21e2f071d59e90e4e9db439f0f9776e
Steven Powell <spowellgrr@gmail.com>
2023-08-10 15:27:20 -0700

    CI: Add cloudbuild deploy script

---
+++ deploy/cloudrun/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+
+export IMAGE=gcr.io/k8s-minikube/dbbrowser
+
+docker buildx build --platform linux/amd64 -t "${IMAGE}" -f Dockerfile.dbbrowser .
+
+docker push "${IMAGE}" || exit 2
+
+gcloud run deploy dbbrowser \
+    --project k8s-minikube \
+    --image "${IMAGE}" \
+    --set-env-vars="DB_HOST=${DB_HOST},DB_PATH=${DB_PATH}" \
+    --allow-unauthenticated \
+    --region us-central1 \
+    --memory 2Gi \
+    --platform managed
